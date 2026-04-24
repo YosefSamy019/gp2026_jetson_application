ad59a3e87966825913cbbe327a0c2cbef1275d41
Youssef Samy <yosefsamy019@gmail.com>
2026-04-24 23:16:50 +0300

    edits

--- models/objects.py
+++ models/objects.py
@@ -9,10 +9,10 @@ _objects_model: Optional[YOLODetector] = None
 
 def init():
     global _objects_model
-    # _objects_model = YOLODetector(
-    #     model_path=assets_manager.OBJECTS_YOLO_MODEL_PATH,
-    #     classes_path=assets_manager.OBJECTS_YOLO_CLASSES_PATH,
-    # )
+    _objects_model = YOLODetector(
+        model_path=assets_manager.OBJECTS_YOLO_MODEL_PATH,
+        classes_path=assets_manager.OBJECTS_YOLO_CLASSES_PATH,
+    )
 
 
 def objects_model(image: np.ndarray):
